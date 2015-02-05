d08aa69adf7949eab930efce2c10391bb5405371
Snayki <Snayki007@mail.ru>
2015-02-05 15:59:53 +0200

    update ui routs

--- js/app.js
+++ js/app.js
@@ -3,3 +3,40 @@ var MainApp = angular.module('MainApp', ['ui.router', 'mgcrea.ngStrap']);
 MainApp.run(function($rootScope, $state, $stateParams, $location, $q) {
 
 });
+
+
+MainApp.config(function($stateProvider, $urlRouterProvider) {
+    //
+    // For any unmatched url, redirect to /state1
+//    $urlRouterProvider.otherwise("/home");
+    //
+    // Now set up the states
+    $stateProvider
+        .state('pms', {
+            url: '',
+            abstract: true,
+            views: {
+                '': {
+                    template: '<div ui-view="" />'
+                }
+            }
+        })
+        .state('pms.home', {
+            url: "/",
+            templateUrl: "html/home.html"
+        })
+        .state('pms.dev', {
+            url: "/dev",
+            templateUrl: "html/main_dev.html",
+            controller: function($scope) {
+                $scope.items = ["A", "List", "Of", "Items"];
+            }
+        })
+        .state('pms.pm', {
+            url: "/pm",
+            templateUrl: "html/main_pm.html",
+            controller: function($scope) {
+                $scope.things = ["A", "Set", "Of", "Things"];
+            }
+        });
+});

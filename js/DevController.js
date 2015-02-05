6e87872ff55ce368992c1750d3d93788922a212f
dmitrysl <Cronos_281435462@#!>
2015-02-05 23:01:06 +0200

    update breadcrambs

--- js/DevController.js
+++ js/DevController.js
@@ -2,8 +2,8 @@ MainApp.controller("DevViewController", function($controller, $scope) {
     $controller('ViewController', {$scope: $scope});
 
     $scope.main.showBreadcrambs = false;
-    $scope.main.currentBreadCrumbItem = null;
-    $scope.main.breadCrumbItems = [];
+    //$scope.main.currentBreadCrumbItem = null;
+    //$scope.main.breadCrumbItems = [];
 
     $scope.filters = [];
 
